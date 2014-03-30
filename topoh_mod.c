2c16d4b5523b77610d40a5e78d972612969beb28
root <root@kamailio.kamailio>
2014-03-30 05:18:37 -0500

    pracovna verzia, pred cistenim

--- topoh_mod.c
+++ topoh_mod.c
@@ -42,6 +42,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #include "../../sr_module.h"
 #include "../../events.h"
@@ -67,6 +68,13 @@ int extract_rtcp(str *body, str *rtcp);
 #include "../../parser/sdp/sdp_helpr_funcs.h"
 
 MODULE_VERSION
+
+
+#define SIP_REQ 1
+#define SIP_REP 2
+#define RTP 3
+#define RTCP 4
+
 
 /* socket handle */
 int handle;
@@ -100,7 +108,7 @@ struct module_exports exports= {
  */
 static int mod_init(void)
 {
-	init_sockets();
+	//init_sockets();
 
 	sr_event_register_cb(SREV_NET_DATA_IN, th_msg_received);
 	sr_event_register_cb(SREV_NET_DATA_OUT, th_msg_sent);
@@ -138,11 +146,11 @@ int get_msg_type(sip_msg_t *msg)
 		//find out if packet is SIP request or reply
 		if (msg->first_line.type == SIP_REQUEST)
 		{
-			return 1;
+			return SIP_REQ;
 		}
 		else if (msg->first_line.type == SIP_REPLY)
 		{
-			return 2;
+			return SIP_REP;
 		}
 		else
 		{
@@ -162,11 +170,11 @@ int get_msg_type(sip_msg_t *msg)
 		//if 6th and 5th bit is "10" it's RTCP else RTP
 		if (!!(byte & BIT6) == 1 && !!(byte & BIT5) == 0)
 		{
-			return 3; //rtcp packet
+			return RTCP;
 		}
 		else
 		{
-			return 4; //rtp packet
+			return RTP;
 		}
 	}
 }
@@ -201,16 +209,142 @@ int get_msg_body(struct sip_msg *msg, str *body)
 }
 
 /** endpoint structure */
-typedef struct
+typedef struct endpoint
 {
 	unsigned short rtp_port;
 	unsigned short rtcp_port;
-	struct sockaddr_in ip_address;
+	char *ip_address;
+	char ip [50];
+
+	/* SIP_REQ or SIP_REP */
+	unsigned short type;
 
 	struct endpoint *next;
 
 } endpoint;
 
+int parseEndpoint(struct sip_msg *msg, endpoint *endpoint)
+{
+	if (parse_sdp(msg) == 0)
+	{
+		str sdp = {0, 0};
+		if (get_msg_body(msg, &sdp) == 0)
+		{
+			str mediamedia;
+			str mediaport;
+			str mediatransport;
+			str mediapayload;
+			int is_rtp;
+
+//			LM_DBG("sdp: %s", sdp.s);
+
+
+			if (extract_media_attr(&sdp, &mediamedia, &mediaport, &mediatransport, &mediapayload, &is_rtp) == 0)
+			{
+				char tmp[mediaport.len];
+				memcpy(tmp, mediaport.s, mediaport.len);
+
+				endpoint->rtp_port = atoi(tmp);
+			}
+			else
+			{
+				LM_ERR("Cannot parse RTP port.");
+
+				return -1;
+			}
+
+			str rtcp;
+
+			if (extract_rtcp(&sdp, &rtcp) == 0)
+			{
+				char tmp[rtcp.len];
+				memcpy(tmp, rtcp.s, rtcp.len);
+
+				endpoint->rtcp_port = atoi(tmp);
+			}
+			else
+			{
+				endpoint->rtcp_port = endpoint->rtp_port + 2;
+			}
+
+			LM_DBG("sdp: %s", sdp.s);
+
+			unsigned int a, b, c, d;
+			char *creator;
+
+			creator = strstr(sdp.s, "c=IN IP4 ");
+
+			sscanf(creator + 9, "%d.%d.%d.%d", &a, &b, &c, &d);
+
+			char ip [50];
+
+			LM_DBG("before sprintf");
+			int len = sprintf(ip, "%d.%d.%d.%d", a, b, c, d);
+
+			if (len > 0)
+			{
+				strcpy(endpoint->ip, ip);
+			}
+			else
+			{
+				LM_ERR("Cannot parse media IP");
+
+				return -1;
+			}
+
+			//ip = endpoint->ip_address;
+
+			//endpoint->ip_address = ip;
+			//LM_DBG("after sprintf %s, %s", ip, endpoint->ip_address);
+
+
+			/*str mediaip;
+			int pf;
+			char line;
+
+			if (extract_mediaip(&sdp, &mediaip, &pf, &line) == 1)
+			{
+				char tmp[mediaip.len];
+				memcpy(tmp, mediaip.s, mediaip.len);
+
+				endpoint->ip_address = tmp;
+			}
+			else
+			{
+				LM_DBG("MEDIA EXTRACT FAILED");
+				return -1;
+			}*/
+
+
+			return 0;
+		}
+		else
+		{
+			LM_ERR("Cannot get message body");
+
+			return -1;
+		}
+	}
+	else
+	{
+		LM_ERR("Cannot parse SDP.");
+
+		return -1;
+	}
+}
+
+
+void printEndpoint(endpoint *endpoint)
+{
+	LM_DBG("Endpoint:\n\tIP: %s\n\tRTP: %d\n\tRTCP: %d\n\tOrigin: %s\n\n",
+		endpoint->ip,
+		endpoint->rtp_port,
+		endpoint->rtcp_port,
+		endpoint->type == SIP_REQ ?
+			"SIP_REQUEST" :
+			"SIP_REPLY"
+	);
+}
 
 /**
  *
@@ -221,16 +355,13 @@ int th_msg_received(void *data)
 	str *obuf;
 	char *nbuf = NULL;
 
-        unsigned short rtp_port = 0;
-        unsigned short rtcp_port = 0;
-
-
 	obuf = (str*)data;
 	memset(&msg, 0, sizeof(sip_msg_t));
 	msg.buf = obuf->s;
 	msg.len = obuf->len;
 
 	LM_DBG("\n\n########## MESSAGE\n\n%s\n##########\n\n", obuf->s);
+	return 0;
 
 	int msg_type = get_msg_type(&msg);
 
@@ -238,61 +369,26 @@ int th_msg_received(void *data)
 
 	switch (msg_type)
 	{
-		case 1:
-			//parse RTP/RTCP ports
-			LM_DBG("####\n\nMessage type SIP REQUEST\n\n###");
-
-			if (parse_sdp(&msg) == 0)
+		case SIP_REQ: //no break
+		case SIP_REP:
 			{
-				LM_DBG("sdp parsed");
+				//parse RTP/RTCP ports
+				endpoint ep;
 
-				str sdp = {0, 0};
-				unsigned short rtpPort;
-				unsigned short rtcpPort;
-
-				if (get_msg_body(&msg, &sdp) == 0)
+				if (parseEndpoint(&msg, &ep) == 0)
 				{
-					LM_DBG("####\n\nASDF PARSED SDP:\n\n%s\n\n###\n", sdp.s);
+//					LM_DBG("\n\n#####\n\nRTP: %d\nRTCP: %d\n\n#####\n\n", ep.rtp_port, ep.rtcp_port);
 
-					str mediamedia;
-					str mediaport;
-					str mediatransport;
-					str mediapayload;
-					int is_rtp;
-					str rtcp;
-
-					if (extract_media_attr(&sdp, &mediamedia, &mediaport, &mediatransport, &mediapayload, &is_rtp) == 0)
-					{
-						//LM_DBG("mediaport: %s\n\n%d\n####\n\n", mediaport.s, mediaport.len);
-
-						char tmp[mediaport.len];
-						memcpy(tmp, mediaport.s, mediaport.len);
-
-						rtpPort = atoi(tmp);
-					}
-
-					if (extract_rtcp(&sdp, &rtcp) == 0)
-					{
-						char tmp[rtcp.len];
-						memcpy(tmp, rtcp.s, rtcp.len);
-
-						rtcpPort = atoi(tmp);
-					}
-					else
-					{
-						rtcpPort = rtpPort + 2;
-					}
-
-					LM_DBG("rtpPort: %d\nrtcpPort: %d\n", rtpPort, rtcpPort);
-
+					ep.type = msg_type;
+					printEndpoint(&ep);
 				}
+				else
+				{
+					LM_DBG("failed to parse endpoint");
+				}
+
+				break;
 			}
-
-
-			break; case 2:
-			//parse RTP/RTCP ports
-			LM_DBG("Message type SIP REPLY");
-			break;
 		case 3: //no break
 		case 4:
 			LM_DBG("Message type RTP/RTCP");
