54c40d33459ab2122c47eaf7838b086acc4cfd89
Ivan Barlog <ivan@barlog.sk>
2016-04-21 17:12:40 +0200

    add checking for REGISTER functions

--- sip_single_port/ssp_parse.c
+++ sip_single_port/ssp_parse.c
@@ -80,3 +80,43 @@ int cancels_dialog(sip_msg_t *msg) {
 
     return -1;
 }
+
+int is_register_request(sip_msg_t *msg) {
+    if (parse_msg(msg->buf, msg->len, msg) == 0) {
+
+        if (msg->first_line.type == SIP_REQUEST) {
+
+            if (msg->REQ_METHOD == METHOD_REGISTER) {
+                return 0;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int is_register_response(sip_msg_t *msg) {
+    if (parse_msg(msg->buf, msg->len, msg) == 0) {
+
+        if (msg->first_line.type == SIP_REPLY) {
+
+            if (parse_headers(msg, HDR_CSEQ_F, 0) != 0) {
+                return -1;
+            }
+
+            if (&msg->cseq == NULL || &msg->cseq->body.s == NULL) {
+                return -1;
+            }
+
+            unsigned int code = msg->REPLY_STATUS;
+            if (code >= 200 && code < 300 && get_cseq(msg)->method_id == METHOD_REGISTER) {
+                return 0;
+            }
+        }
+    }
+
+    return -1;
+}
+
+
+
