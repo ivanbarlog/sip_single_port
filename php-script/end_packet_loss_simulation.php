5e110d5fd9baffc4f260a2bc6f46ab56f5388339
Ivan Barlog <ivan@barlog.sk>
2016-05-06 22:09:57 +0200

    remove simulation of packet loss - will be executed by changing IPTABLES

--- php-script/end_packet_loss_simulation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (PHP_SAPI !== 'cli') {
-    die("script must be run from PHP CLI\n");
-}
-
-$path = sprintf("%s%s..%s%s", dirname(__FILE__), DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, "packet_loss");
-
-unlink($path);
--- php-script/simulate_packet_loss.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (PHP_SAPI !== 'cli') {
-    echo "script must be run from PHP CLI\n";
-}
-
-$path = sprintf("%s%s..%s%s", dirname(__FILE__), DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, "packet_loss");
-
-echo (int)file_exists($path);
